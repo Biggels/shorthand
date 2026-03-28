c4202b1512370a3775df3c2ecd0879ee4e124856
Ray <haackra@gmail.com>
2026-03-28 11:18:57 -0400

    add word lists

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
         # dict of startswith sets
         # dict of endswith sets
     # this way we can say things like "words of length 5 that start with b and end with e"
+    #
+    # let's start with the oxford 3000, and then maybe do 3of6game or 2of12inf
 
 
 if __name__ == "__main__":
