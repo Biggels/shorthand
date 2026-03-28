52af963ac59a200602b29b504cca588e0c133286
Ray <haackra@gmail.com>
2026-03-28 09:27:19 -0400

    uv init, some brainstorming

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    # sets:
+        # dict of length sets
+        # dict of startswith sets
+        # dict of endswith sets
+    # this way we can say things like "words of length 5 that start with b and end with e"
+
+
+if __name__ == "__main__":
+    main()
