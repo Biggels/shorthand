51f84b6743ac5ac6490f197233da426e290c2a84
Biggels <haackra@gmail.com>
2026-04-23 19:53:04 -0400

    do the whole oxford 3000!

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 def main():
     # let's start with the oxford 3000, and then maybe do 3of6game or 2of12inf
+    words = set()
+    with open("word_lists/oxford_3000.txt", "r") as file:
+        for word in file.readlines():
+            word = word.strip("\n").lower()
+            words.add(word)
 
-    word_set = {"bicycle", "because", "balance", "breathe", "believe"}
-    result = shorten_set(word_set)
+    # word_set = {"bicycle", "because", "balance", "breathe", "believe"}
+    result = shorten_words(words, middle_threshold=2)
     print({word: result[word] for word in sorted(result)})
     return
 
 
-def shorten(word, reveal_from_left=1, reveal_from_right=1, middle_threshold=1):
+def shorten_word(word, reveal_from_left=1, reveal_from_right=1, middle_threshold=1):
     middle_length = len(word) - reveal_from_left - reveal_from_right
     if middle_length <= middle_threshold:
         return word
     return word[:reveal_from_left] + str(middle_length) + word[-reveal_from_right:]
 
 
-def shorten_set(word_set, reveal_from_left=1, reveal_from_right=1, middle_threshold=1):
+def shorten_words(
+    word_set, reveal_from_left=1, reveal_from_right=1, middle_threshold=1
+):
     words = sorted(word_set)
     shorthand_map = {
-        word: shorten(
+        word: shorten_word(
             word=word,
             reveal_from_left=reveal_from_left,
             reveal_from_right=reveal_from_right,
@@ -44,7 +51,7 @@ def shorten_set(word_set, reveal_from_left=1, reveal_from_right=1, middle_thresh
             else:
                 next_reveal_from_right += 1
             result.update(
-                shorten_set(
+                shorten_words(
                     word_set=words,
                     reveal_from_left=next_reveal_from_left,
                     reveal_from_right=next_reveal_from_right,
