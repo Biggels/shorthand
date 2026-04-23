347fab0dbadbc2fb89a36874c6905dbb5f5c4a4c
Biggels <haackra@gmail.com>
2026-04-23 13:32:15 -0400

    add a function to turn word into shorthand

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     # this way we can say things like "words of length 5 that start with b and end with e"
     #
     # let's start with the oxford 3000, and then maybe do 3of6game or 2of12inf
+    #
+    print(shorten("palace", 4))
+    return
 
     WORDS_BY_LENGTH = {}
     WORDS_BY_STARTS_WITH = {}
@@ -23,6 +26,13 @@ def main():
             WORDS_BY_ENDS_WITH.setdefault(ends_with, set()).add(word)
 
     print(WORDS_BY_LENGTH[5] & WORDS_BY_STARTS_WITH["b"] & WORDS_BY_ENDS_WITH["e"])
+
+
+def shorten(word, reveal_from_left=1, reveal_from_right=1, middle_treshold=1):
+    middle_length = len(word) - reveal_from_left - reveal_from_right
+    if middle_length <= middle_treshold:
+        return word
+    return word[:reveal_from_left] + str(middle_length) + word[-reveal_from_right:]
 
 
 if __name__ == "__main__":
